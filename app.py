73a6fc56c2aca68876c61a42403113cde643c0b5
nhu220840 <nhu220840@gmail.com>
2025-06-14 00:02:42 +0700

    change lib version, add port to build dockerfile

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def predict():
     return render_template('predict.html', result=gpa_pred)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
